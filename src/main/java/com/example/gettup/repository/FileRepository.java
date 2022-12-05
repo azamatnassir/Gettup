88b4b0a960385ad356d8aecb891bdd2be3e744be
azamatnassir <azamatnsr@gmail.com>
2022-12-05 14:20:48 +0600

    Revert "Merge pull request #1 from azamatnassir/master"
    This reverts commit 39238153170d77c4b4105b95213747e4d18df194, reversing
    changes made to fe69b83f23a6ab98932c24a47171dfe9ea4512d5.

--- src/main/java/com/example/gettup/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gettup.repository;
-
-import com.example.gettup.entity.FileModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository extends JpaRepository<FileModel, Long> {
-}
