56f562ac92d70a1dcd38d5d0fd34666157be5d28
azamatnassir <azamatnsr@gmail.com>
2022-12-05 14:23:34 +0600

    1

---
+++ src/main/java/com/example/gettup/GettupApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettupApplication.class, args);
+	}
+
+}
